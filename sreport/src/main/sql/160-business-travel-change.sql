48d994dba4c437407a4de1936302fd63daaf75d8
Tim Stephenson <tim@knowprocess.com>
2018-01-04 17:53:32 +0000

    fix file name and content of #160 update

--- sreport/src/main/sql/160-business-travel-change.sql
+++ sreport/src/main/sql/160-business-travel-change.sql
@@ -1,8 +1,8 @@
 update SR_QUESTION 
   set label = 'Business Mileage - Non-organisation Owned Grey Fleet/Pool/Third party provided used by the organisation (excluding leased vehicles) Road Travel (Hire etc.)'
-  where name = 'BIZ_MILEAGE_ROADS';
-update SR_QUESTION set name = 'BIZ_MILEAGE_ROADS_COST',
+  where name = 'BIZ_MILEAGE_ROAD';
+update SR_QUESTION set name = 'BIZ_MILEAGE_ROAD_COST',
   label = 'Cost of Business Mileage - Non-organisation Grey Owned Fleet/Pool/Third party provided used by the organisation (excluding leased vehicles) Road Travel (Hire etc.)',
   unit = '£'
   where name = '_3RD_PTY_ROAD_MILES';
- update SR_CAT set questions = replace(questions, '_3RD_PTY_ROAD_MILES','BIZ_MILEAGE_ROADS_COST');
+ update SR_CAT set questions = replace(questions, '_3RD_PTY_ROAD_MILES','BIZ_MILEAGE_ROAD_COST');
