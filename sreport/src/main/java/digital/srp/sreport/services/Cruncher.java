f8c21c6ebce37e76083a6aef08bfb9ef97ecaa3c
Tim Stephenson <tim@knowprocess.com>
2020-12-07 21:12:18 +0000

    implement fallback for cfactors as wfactors

--- sreport/src/main/java/digital/srp/sreport/services/Cruncher.java
+++ sreport/src/main/java/digital/srp/sreport/services/Cruncher.java
@@ -1257,7 +1257,16 @@ public class Cruncher implements digital.srp.sreport.model.surveys.SduQuestions,
                 return cfactor;
             }
         }
-        LOGGER.error("Unable to find Carbon Factor {} for period {}", cfName, period);
+        LOGGER.info(
+                "Unable to find exact Carbon Factor, now looking for {} alone",
+                cfName);
+        for (CarbonFactor cfactor : cfactors) {
+            if (cfName.name().equals(cfactor.category())) {
+                return cfactor;
+            }
+        }
+        LOGGER.error("Unable to find Carbon Factor {} for any period",
+                cfName, period);
         throw new ObjectNotFoundException(CarbonFactor.class, cfName.name());
     }
 
