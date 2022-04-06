0da56178ce547932d6e83c0727795d0f75a9bab3
Tim Stephenson <tim@knowprocess.com>
2022-04-06 14:00:34 +0100

    unfortunately cannot rely on $env.period being loaded in time

--- srp-ui/src/srp/js/report-2020-21.js
+++ srp-ui/src/srp/js/report-2020-21.js
@@ -25,9 +25,9 @@ var ractive = new BaseRactive({
         'PROVIDER7_COMMISSIONED','PROVIDER8_COMMISSIONED','CCG1_SERVED',
         'CCG2_SERVED','CCG3_SERVED','CCG4_SERVED','CCG5_SERVED','CCG6_SERVED','CCG7_SERVED','CCG8_SERVED'],
     requiredAnswers: ['ORG_CODE', 'ORG_NAME', 'ORG_TYPE', 'SDMP_CRMP', 'HEALTHY_TRANSPORT_PLAN', 'PROMOTE_HEALTHY_TRAVEL'],
-    period: $env.period,
+    period: '2021-22',
     server: $env.server,
-    survey: 'SDU-'+$env.period,
+    survey: 'SDU-2021-22',
     tenant: { id: 'sdu' },
     username: localStorage.getItem('username'),
     formatAbsAnswer: function(qName, period) {
